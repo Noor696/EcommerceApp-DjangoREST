082388d67e63036a8a52b9248130162a6308b384
Noor696 <eng.nooralhateeb@gmail.com>
2022-12-13 08:57:23 +0000

    view,create,update,delete object

--- schools/urls.py
+++ schools/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path  
-from .views import SchoolListView
+from .views import SchoolListView , SchooleDetailView  
+
 urlpatterns = [
     path('', SchoolListView.as_view(),name="school_list"),
+    path('<int:pk>', SchooleDetailView.as_view(),name='school_detail')
 ]
+
+#SchooleDetailView
--- schools/views.py
+++ schools/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from .serializers  import SchoolSerializer
 from .models import School
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView ,ListCreateAPIView, RetrieveAPIView , RetrieveUpdateAPIView , RetrieveUpdateDestroyAPIView
 
 # Create your views here.
-class SchoolListView(ListAPIView):
-    # I want to show user the data
+class SchoolListView(ListCreateAPIView):
+    # I want to show user the data and can create another data from list
     queryset = School.objects.all() # take all object and give him to serilizer
     serializer_class = SchoolSerializer # afile resposible to convert python code to JSON data/ format
+
+# class SchoolListView(ListAPIView):
+#     # I want to show user the data
+#     queryset = School.objects.all() 
+#     serializer_class = SchoolSerializer 
+
+class SchooleDetailView(RetrieveUpdateDestroyAPIView):
+
+    # RetrieveUpdateDestroyAPIView : mean reading , can Updating and delet
+    queryset = School.objects.all() 
+    serializer_class = SchoolSerializer 
+
+# class SchooleDetailView(RetrieveUpdateAPIView):
+
+#     # RetrieveUpdateAPIView : mean reading and can editing
+#     queryset = School.objects.all() 
+#     serializer_class = SchoolSerializer 
+
+# class SchooleDetailView(RetrieveAPIView):
+
+#     # RetrieveAPIView : mean just reading the data
+#     queryset = School.objects.all() 
+#     serializer_class = SchoolSerializer 
